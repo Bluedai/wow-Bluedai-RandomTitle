675a9c8d3e02a8c8459f4761f8984215e27155a5
Bluedai <gethub.232012@bd4u.de>
2024-08-30 18:58:25 +0200

    Popup with new title
    Popup message with new title

--- core.lua
+++ core.lua
@@ -19,6 +19,23 @@ local function LoadTitles()
 end
 Bluedai_RT_Functions.LoadTitles = LoadTitles
 
+local function show_new_title()
+    if Bluedai_RT_Variables.show and Bluedai_RT.EnabledshownewTitle then
+        local new_title = UnitPVPName("player")
+        local frame = CreateFrame("ScrollingMessageFrame", nil, UIParent)
+        frame:SetSize(1100, 75)
+        frame:SetPoint("CENTER", UIParent, "CENTER", 0, 200)
+        frame:SetFont(STANDARD_TEXT_FONT, 24, "OUTLINE")
+        local r, g, b = 0, 1, 1 -- textcolor red, green, blue between 0 and 1
+        frame:SetFading(true)
+        frame:SetFadeDuration(1)
+        frame:SetTimeVisible(3)
+        frame:AddMessage( new_title , r,g,b )
+    end
+    Bluedai_RT_Variables.show = false
+end
+Bluedai_RT_Functions.show_new_title = show_new_title
+
 local function SetRandomTitle()
     local eligibleIDs = {}  -- A table to store valid IDs
     local currentTitleID = GetCurrentTitle()
@@ -36,8 +53,8 @@ local function SetRandomTitle()
         end
     end
     if #eligibleIDs > 0 then
+        Bluedai_RT_Variables.show = true
         local randomIndex = math.random(1, #eligibleIDs)
-        -- print("debug: RandomTitleID : " .. eligibleIDs[randomIndex] .. "  Titel:" .. GetTitleName(eligibleIDs[randomIndex]) )
         SetCurrentTitle(eligibleIDs[randomIndex])
     end
 end
